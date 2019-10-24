0b36960e3f4d4516503cbc8fa442f089a7405e0a
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 03:36:06 +0200

    loop success

--- designs.js
+++ designs.js
@@ -4,17 +4,19 @@ window.onload=function(){
   
   document.getElementById("submit").addEventListener("click", makeGrid);
   
-  function makeGrid(height,width) {
-    const gridHeight = document.querySelector( "#inputHeight" ).value ;
-    const gridWidth = document.querySelector( "#inputWidth" ).value ;
+  function makeGrid() {
+    let gridHeight = document.querySelector( "#inputHeight" ).value ;
+    let gridWidth = document.querySelector( "#inputWidth" ).value ;
+    for (let i = 0 ; i < gridHeight ; i++){
     const pixelCanvas = document.getElementById("pixelCanvas");
     let Row = document.createElement("tr");
     let createRow = pixelCanvas.appendChild(Row);
+    for (let i = 0 ; i < gridWidth ; i++){
       (function createCell(){
         let Cell = document.createElement("td");
         let createCell = Row.appendChild(Cell);
-        let data = document.createTextNode(gridWidth+gridHeight);
+        let data = document.createTextNode("X");
         Cell.appendChild(data);
-      })();
+      })();}}
     }
   }
