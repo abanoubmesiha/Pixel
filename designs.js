715fa9a6612a4bfc16a8f6bc77d5c6be61c88ece
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 04:44:24 +0200

    comments

--- designs.js
+++ designs.js
@@ -3,25 +3,27 @@ window.onload=function(){
   document.getElementById("submit").addEventListener("click", makeGrid);
   
   function makeGrid() {
-
+// deleting the table before submiting a new one
     const pixelCanvas = document.getElementById("pixelCanvas");
-    let tableRows = pixelCanvas.getElementsByTagName('tr');
+    const tableRows = pixelCanvas.getElementsByTagName('tr');
     let rowCount = tableRows.length;
 
     for (let x = rowCount -1; x >= 0 ; x--) {
       pixelCanvas.removeChild(tableRows[x]);
     }
-
-    let gridHeight = document.querySelector("#inputHeight").value ;
-    let gridWidth = document.querySelector("#inputWidth").value ;
+// making the rows
+    const gridHeight = document.querySelector("#inputHeight").value ;
+    const gridWidth = document.querySelector("#inputWidth").value ;
 
     for (let i = 0 ; i < gridHeight ; i++){
-        let Row = document.createElement("tr");
-        let createRow = pixelCanvas.appendChild(Row);
-            for (let i = 0 ; i < gridWidth ; i++){
+      const Row = document.createElement("tr");
+      const createRow = pixelCanvas.appendChild(Row);
+        // making the cells of every row
+            for (let i = 0 ; i < gridWidth ; i++){ 
               (function createCell(){
-                let Cell = document.createElement("td");
-                let createCell = Row.appendChild(Cell);
+                const Cell = document.createElement("td");
+                const createCell = Row.appendChild(Cell);
+                // function of coloring the cells
                 Cell.addEventListener('mousedown', function() {
                   const color = document.querySelector("#colorPicker").value;
                   this.style.backgroundColor = color;
