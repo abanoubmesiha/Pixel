8cc75ea23c24d3dfbc46c27b3a5c272e9f6c87e1
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 19:48:45 +0200

    start with a grid

--- designs.js
+++ designs.js
@@ -5,29 +5,25 @@ window.onload=function(){
   function makeGrid() {
 // deleting the table before submiting a new one
     const pixelCanvas = document.getElementById("pixelCanvas");
-    const tableRows = pixelCanvas.getElementsByTagName('tr');
-    let rowCount = tableRows.length;
-
-    for (let x = rowCount -1; x >= 0 ; x--) {
-      pixelCanvas.removeChild(tableRows[x]);
-    }
+    while (pixelCanvas.firstChild) 
+    pixelCanvas.removeChild(pixelCanvas.firstChild);
 // making the rows
-    const gridHeight = document.querySelector("#inputHeight").value ;
-    const gridWidth = document.querySelector("#inputWidth").value ;
+    let gridHeight = document.querySelector("#inputHeight").value;
+    let gridWidth = document.querySelector("#inputWidth").value;
 
     for (let i = 0 ; i < gridHeight ; i++){
       const Row = document.createElement("tr");
-      const createRow = pixelCanvas.appendChild(Row);
+      pixelCanvas.appendChild(Row);
         // making the cells of every row
             for (let i = 0 ; i < gridWidth ; i++){ 
-              (function createCell(){
                 const Cell = document.createElement("td");
-                const createCell = Row.appendChild(Cell);
+                Row.appendChild(Cell);
                 // function of coloring the cells
                 Cell.addEventListener('mousedown', function() {
                   const color = document.querySelector("#colorPicker").value;
                   this.style.backgroundColor = color;
                 })
-      })();}}
+      }}
     }
+    makeGrid();
   }
