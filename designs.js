7ccf57df0dc35b33312880028d23389325b2a739
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 04:10:17 +0200

    basics are finished

--- designs.js
+++ designs.js
@@ -1,10 +1,9 @@
 window.onload=function(){
   
-  //console.log(gridHeight,gridWidth);
-  
   document.getElementById("submit").addEventListener("click", makeGrid);
   
   function makeGrid() {
+
     const pixelCanvas = document.getElementById("pixelCanvas");
     let tableRows = pixelCanvas.getElementsByTagName('tr');
     let rowCount = tableRows.length;
@@ -12,17 +11,21 @@ window.onload=function(){
     for (let x = rowCount -1; x >= 0 ; x--) {
       pixelCanvas.removeChild(tableRows[x]);
     }
+
     let gridHeight = document.querySelector("#inputHeight").value ;
     let gridWidth = document.querySelector("#inputWidth").value ;
+
     for (let i = 0 ; i < gridHeight ; i++){
-    let Row = document.createElement("tr");
-    let createRow = pixelCanvas.appendChild(Row);
-    for (let i = 0 ; i < gridWidth ; i++){
-      (function createCell(){
-        let Cell = document.createElement("td");
-        let createCell = Row.appendChild(Cell);
-        let data = document.createTextNode("X");
-        Cell.appendChild(data);
+        let Row = document.createElement("tr");
+        let createRow = pixelCanvas.appendChild(Row);
+            for (let i = 0 ; i < gridWidth ; i++){
+              (function createCell(){
+                let Cell = document.createElement("td");
+                let createCell = Row.appendChild(Cell);
+                Cell.addEventListener('mousedown', function() {
+                  const color = document.querySelector("#colorPicker").value;
+                  this.style.backgroundColor = color;
+                })
       })();}}
     }
   }
