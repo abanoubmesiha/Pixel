61a55e80fe17082880765385025fa742ee3334a2
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 03:59:32 +0200

    remove previous table function

--- designs.js
+++ designs.js
@@ -5,10 +5,16 @@ window.onload=function(){
   document.getElementById("submit").addEventListener("click", makeGrid);
   
   function makeGrid() {
-    let gridHeight = document.querySelector( "#inputHeight" ).value ;
-    let gridWidth = document.querySelector( "#inputWidth" ).value ;
-    for (let i = 0 ; i < gridHeight ; i++){
     const pixelCanvas = document.getElementById("pixelCanvas");
+    let tableRows = pixelCanvas.getElementsByTagName('tr');
+    let rowCount = tableRows.length;
+
+    for (let x = rowCount -1; x >= 0 ; x--) {
+      pixelCanvas.removeChild(tableRows[x]);
+    }
+    let gridHeight = document.querySelector("#inputHeight").value ;
+    let gridWidth = document.querySelector("#inputWidth").value ;
+    for (let i = 0 ; i < gridHeight ; i++){
     let Row = document.createElement("tr");
     let createRow = pixelCanvas.appendChild(Row);
     for (let i = 0 ; i < gridWidth ; i++){
