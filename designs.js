692b00d8097d12358129e3958d79bdffb5eb3f92
abanoubmesiha <bony.nagy@gmail.com>
2019-10-24 03:14:44 +0200

    row ,cell, data creation function

--- designs.js
+++ designs.js
@@ -1,9 +1,20 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-  // Your code goes here!
-}
-
+window.onload=function(){
+  
+  //console.log(gridHeight,gridWidth);
+  
+  document.getElementById("submit").addEventListener("click", makeGrid);
+  
+  function makeGrid(height,width) {
+    const gridHeight = document.querySelector( "#inputHeight" ).value ;
+    const gridWidth = document.querySelector( "#inputWidth" ).value ;
+    const pixelCanvas = document.getElementById("pixelCanvas");
+    let Row = document.createElement("tr");
+    let createRow = pixelCanvas.appendChild(Row);
+      (function createCell(){
+        let Cell = document.createElement("td");
+        let createCell = Row.appendChild(Cell);
+        let data = document.createTextNode(gridWidth+gridHeight);
+        Cell.appendChild(data);
+      })();
+    }
+  }
